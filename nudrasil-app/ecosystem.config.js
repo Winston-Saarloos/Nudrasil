43ef28c013cf9da1b17479b5523a336a97390dec
97rocko <97rocko@gmail.com>
2025-04-16 02:44:34 -0500

    dropped pm2 usage at build (runner service restriction on Windows)

--- nudrasil-app/ecosystem.config.js
+++ nudrasil-app/ecosystem.config.js
@@ -4,11 +4,13 @@ module.exports = {
       name: "nextjs-app",
       script: "cmd",
       args: "/c yarn start",
+      cwd: "C:/GitHubRunners/actions-runner/_work/Nudrasil/Nudrasil/nudrasil-app",
+      watch: ["./.next"],
+      ignore_watch: ["node_modules", ".git", ".yarn"],
       env: {
         NODE_ENV: "production",
         PORT: 3000,
       },
-      node_args: "--no-daemon"
     },
   ],
 };
