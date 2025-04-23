53bca0c8e74ffbb116b071fda248badb550b6e8c
97rocko <97rocko@gmail.com>
2025-04-23 02:55:18 -0500

    updated deployment to include .env

--- nudrasil-app/scripts/deploy.ps1
+++ nudrasil-app/scripts/deploy.ps1
@@ -1,6 +1,8 @@
 $deployPath = "C:\Deploy\nudrasil-live"
 $source = "$PSScriptRoot\.."
 $build = "$source\.next\standalone"
+$envSource = "C:\Deploy\.env.production"
+$envDest = "$deployPath\.env"
 
 Write-Host ">> Stop old app (if running)..."
 pm2 stop nextjs-app
@@ -18,6 +20,10 @@ Copy-Item "$build\node_modules" -Destination "$deployPath\node_modules" -Recurse
 Copy-Item "$source\.next\static" -Destination "$deployPath\.next\static" -Recurse -Force
 Copy-Item "$source\public" -Destination "$deployPath\public" -Recurse -Force
 Copy-Item "$source\ecosystem.config.cjs" -Destination $deployPath -Force
+
+# copy environment file into place
+Write-Host ">> Copying environment config (.env.production -> .env)..."
+Copy-Item -Path $envSource -Destination $envDest -Force
 
 Write-Host ">> Start new version with PM2..."
 pm2 start ecosystem.config.cjs --only nextjs-app
