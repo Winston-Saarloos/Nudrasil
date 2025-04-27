4736940983ce5efa6ad00b3c225449526cd83306
Rocko <winston.saarloos@gmail.com>
2025-04-27 05:35:35 -0500

    updated to use config from web server

--- firmware/esp-dht22-sensor/src/main.cpp
+++ firmware/esp-dht22-sensor/src/main.cpp
@@ -1,21 +1,64 @@
-#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
 #include "DHT.h"
-
-const char* ssid = "";
-const char* password = "";
-
-const char* configUrl = "https://app.nudrasil.com/api/admin/device-configs?deviceId=3&key=mySecret";
+#include "secrets.h" 
 
 // DHT sensor config
 #define DHTPIN 4        // GPIO4 (D2 on NodeMCU)
 #define DHTTYPE DHT22   // Can also be DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
+// Server details fetched from config
 String serverIp = "";
 int serverPort = 0;
+
+void fetchServerConfig() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    WiFiClient wifiClient;
+
+    Serial.println("Fetching config from server...");
+    String configUrl = String(SERVER_URL) + String(CONFIG_PATH) + "?secret=" + DEVICE_SECRET + "&deviceId=" + DEVICE_ID;
+
+    http.begin(wifiClient, configUrl);
+    int httpCode = http.GET();
+
+    if (httpCode == 200) {
+      String payload = http.getString();
+
+      // Parse JSON
+      JsonDocument doc;
+      DeserializationError error = deserializeJson(doc, payload);
+
+      if (error) {
+        Serial.print("JSON parse error: ");
+        Serial.println(error.c_str());
+        return;
+      }
+
+      JsonObject device = doc[0];
+      JsonObject config = device["config"];
+      
+      String defaultEnv = config["defaultEnv"] | "prod";
+      JsonObject env = config["environments"][defaultEnv];
+      
+      serverIp = env["ip"].as<String>();
+      serverPort = env["port"].as<int>();
+
+      Serial.println("Using server IP: " + serverIp);
+      Serial.println("Using server Port: " + String(serverPort));
+    } else {
+      Serial.print("Failed to fetch config, HTTP code: ");
+      Serial.println(httpCode);
+    }
+
+    http.end();
+  } else {
+    Serial.println("WiFi not connected when fetching config.");
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -23,7 +66,7 @@ void setup() {
 
   WiFi.setAutoReconnect(true);
   WiFi.persistent(true);
-  WiFi.begin(ssid, password);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   Serial.println("Connecting to WiFi...");
   while (WiFi.status() != WL_CONNECTED) {
@@ -32,51 +75,11 @@ void setup() {
   }
 
   Serial.println("\nConnected to WiFi!");
-  Serial.print("IP address: ");
+  Serial.print("ESP IP address: ");
   Serial.println(WiFi.localIP());
 
-  // fetch config from server
+  // Fetch configuration from server
   fetchServerConfig();
-}
-
-void fetchServerConfig() {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    WiFiClient wifiClient;
-
-    Serial.println("Fetching config from server...");
-    http.begin(wifiClient, configUrl);
-    int httpCode = http.GET();
-
-    if (httpCode == 200) {
-      String payload = http.getString();
-      Serial.println("Received config:");
-      Serial.println(payload);
-
-      // Parse JSON
-      StaticJsonDocument<1024> doc;
-      DeserializationError error = deserializeJson(doc, payload);
-
-      if (error) {
-        Serial.print("JSON parse failed: ");
-        Serial.println(error.c_str());
-      } else {
-        String defaultEnv = doc["defaultEnv"] | "prod";
-        serverIp = doc["environments"][defaultEnv]["ip"].as<String>();
-        serverPort = doc["environments"][defaultEnv]["port"].as<int>();
-
-        Serial.println("Using Server IP: " + serverIp);
-        Serial.println("Using Server Port: " + String(serverPort));
-      }
-    } else {
-      Serial.print("Failed to fetch config. HTTP code: ");
-      Serial.println(httpCode);
-    }
-
-    http.end();
-  } else {
-    Serial.println("WiFi not connected during config fetch.");
-  }
 }
 
 void loop() {
@@ -92,18 +95,16 @@ void loop() {
 
     String serverUrl = "http://" + serverIp + ":" + String(serverPort) + "/api/sensor";
 
+    // --- Post Temperature ---
     HTTPClient http;
     WiFiClient wifiClient;
 
-    // --- Post Temperature ---
     http.begin(wifiClient, serverUrl);
     http.addHeader("Content-Type", "application/json");
     String tempPayload = "{\"sensor\":\"dht22-temp\",\"value\":" + String(t, 2) + "}";
     int httpCode1 = http.POST(tempPayload);
     Serial.println("Temp POST status: " + String(httpCode1));
     http.end();
-
-    delay(1000);
 
     // --- Post Humidity ---
     http.begin(wifiClient, serverUrl);
