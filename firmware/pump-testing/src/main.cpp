7884adb2db2678931495e8a05d1d4690db498256
Rocko <winston.saarloos@gmail.com>
2025-05-15 01:55:14 -0500

    pump sketch

--- firmware/pump-testing/src/main.cpp
+++ firmware/pump-testing/src/main.cpp
@@ -1,21 +1,14 @@
 #include <Arduino.h>
 
-#define PUMP_RELAY_PIN D6  // Use your actual GPIO pin
-
+#define RELAY_PIN D7          // active-LOW
 void setup() {
   Serial.begin(115200);
-  pinMode(PUMP_RELAY_PIN, OUTPUT);
-  
-  // Turn relay ON (active LOW)
-  Serial.println("Pump ON");
-  digitalWrite(PUMP_RELAY_PIN, LOW);  // Relay activated
-  delay(5000);                         // Run pump for 5 seconds
-
-  // Turn relay OFF
-  Serial.println("Pump OFF");
-  digitalWrite(PUMP_RELAY_PIN, HIGH); // Relay deactivated
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);   // relay OFF
 }
-
 void loop() {
-  // Do nothing
+  digitalWrite(RELAY_PIN, LOW);    // ON  (pump runs)
+  delay(3000);
+  digitalWrite(RELAY_PIN, HIGH);   // OFF (pump stops)
+  delay(5000);
 }
